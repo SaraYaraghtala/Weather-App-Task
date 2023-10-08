c07adcb89c2ba5de3cafdbb282335d72a1bef6de
Sara Yaraghtala <sarahyaraghtala@gmail.com>
2023-10-08 22:14:07 +0200

    [feat] Update layout in Layout component

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 import { LayoutProps } from "./Layout.types";
 
 const Layout: FC<LayoutProps> = ({ children }) => {
-  return <div className="bg-iron-200 relative w-full h-screen">{children}</div>;
+  return <div className="bg-iron-200  w-full h-screen flex justify-center items-center ">{children}</div>;
 };
 
 export default Layout;
